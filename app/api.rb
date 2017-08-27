09eea49c1be4116658065f190fb4e012b41dd1c2
Valerie Dryden <outragedpinkracoon@gmail.com>
2017-08-27 18:18:23 +0100

    Removes uneccessary refactor

--- app/api.rb
+++ app/api.rb
@@ -11,11 +11,17 @@ module ExpenseTracker
     post '/expenses' do
       expense = JSON.parse(request.body.read)
       result = @ledger.record(expense)
-      JSON.generate('expense_id' => result.expense_id)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
     end
 
     get '/expenses/:date' do
-      JSON.generate([])
+      JSON.generate []
     end
   end
 end
